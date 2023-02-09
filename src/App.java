809ebccdb2dbf5de580856bc48ab065f820e1121
PROFESSOR D18 <professord18@acad.unifor.br>
2023-02-09 10:53:14 -0300

    Implementado adicionarPosicao

--- src/App.java
+++ src/App.java
@@ -2,7 +2,22 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Lista lista = new Lista();
+
+        System.out.println(lista.estaCheia());
+        System.out.println(lista.estaVazia());
+
+        lista.adicionarPosicao(7, 0);
+        lista.adicionarPosicao(3, 1);
+        lista.adicionarPosicao(2, 2);
+        lista.adicionarPosicao(1, 3);
+        lista.adicionarPosicao(8, 4);
         
-        
+        lista.adicionarPosicao(6, 3);
+
+        System.out.println(lista.estaCheia());
+        System.out.println(lista.estaVazia());
+
+        lista.exibir();
     }
 }
--- src/Lista.java
+++ src/Lista.java
@@ -1,9 +1,4 @@
-// Inserir um elemento e na posição i – adicionarPosicao(e, i)
-// Inserir um elemento e no início – adicionarInicio(e)
-// Inserir um elemento e no final – adicionarFinal(e)
-// Remover o elemento e na posição i – removerPosicao(i)
-// Remover o elemento no inicio – removerInicio(i)
-// Remover o elemento no final – removerFinal(i)
+import javax.swing.JOptionPane;
 
 public class Lista {
     private int[] elementos;
@@ -50,4 +45,51 @@ public class Lista {
         return elementos[indice];
     }
 
+    private boolean validarEntrada(int indice) {
+        if(indice >= 0 || indice <= quantidade)
+            return true;
+        else
+            return false;
+    }
+
+    // Inserir um elemento e na posição i – adicionarPosicao(e, i)
+    public void adicionarPosicao(int elemento, int indice) {
+        if(estaCheia())
+            System.out.println("Não foi possível adicionar pois a lista está cheia!");
+        else {
+            if(!validarEntrada(indice))
+                System.out.println("Posição inválida!");
+            else {
+                int elementoAtual;
+                //afastar alguns elementos
+                for(int i = indice; i <= quantidade; i++) {
+                    elementoAtual = elementos[i];
+    
+                    elementos[i] = elemento;
+    
+                    elemento = elementoAtual;
+                }
+                
+                quantidade++;
+            }
+        }
+    }
+    // Inserir um elemento e no início – adicionarInicio(e)
+    // Inserir um elemento e no final – adicionarFinal(e)
+    // Remover o elemento e na posição i – removerPosicao(i)
+    // Remover o elemento no inicio – removerInicio(i)
+    // Remover o elemento no final – removerFinal(i)
+
+    //Exibir todos os elementos
+    public void exibir(){
+        String elementos = this.elementos[0] + ", ";
+        for (int i = 1; i < quantidade; i++) {
+            elementos += this.elementos[i];
+
+            if(i != (quantidade - 1))
+                elementos += ", ";
+        }
+
+        JOptionPane.showMessageDialog(null, elementos);
+    }
 }
