707468d3fc4145ea00bf08112d113ea57ddc946e
PROFESSOR D18 <professord18@acad.unifor.br>
2023-03-07 11:08:46 -0300

    Implementando Lista Dinamica

--- src/App.java
+++ src/App.java
@@ -1,6 +1,25 @@
+import dinamico.ListaEncadeada;
+import dinamico.No;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        ListaEncadeada lista = new ListaEncadeada();
+
+        System.out.println("Esta vazia (inicio): "+lista.estaVazia());
+        System.out.println("Quantidade (inicio): "+lista.getQuantidade());
+
+        lista.adicionarInicio(7);
+        lista.adicionarFinal(2);
+        lista.adicionarFinal(1);
+
+        lista.exibir();
+        lista.adicionarPosicao(3, 1);
+        lista.adicionarPosicao(12, 12);
+        lista.adicionarPosicao(12, 4);
+        lista.adicionarPosicao(9, 0);
+        lista.exibir();
+
+        System.out.println("Esta vazia (fim): "+lista.estaVazia());
+        System.out.println("Quantidade (fim): "+lista.getQuantidade());
     }
 }
---
+++ src/dinamico/ListaEncadeada.java
@@ -0,0 +1,113 @@
+package dinamico;
+
+import java.util.Arrays;
+
+import javax.swing.JOptionPane;
+
+public class ListaEncadeada {
+    
+    private No primeiro;
+    private int quantidade;
+
+    // Criar uma nova lista – Lista()
+    public ListaEncadeada() {
+        primeiro = null;
+        quantidade = 0;
+    }
+
+    public ListaEncadeada(No primeiro) {
+        this.primeiro = primeiro;
+        quantidade = 1;
+    }
+
+    // Retornar a quantidade de elementos – getQuantidade()
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    // Verificar se a lista está vazia – estaVazia()
+    public boolean estaVazia() {
+        if(quantidade > 0)
+            return false;
+        else
+            return true;
+    }
+
+    // Acessar o elemento na posição i – retornarElemento(i)
+    public int retornarElemento(int indice) {
+        No noAtual = primeiro;
+
+        for (int i = 0; i < indice; i++)
+            noAtual = noAtual.getProximo();
+
+        return noAtual.getElemento();
+    }
+
+    private boolean validarEntrada(int indice) {
+        if(indice >= 0 || indice <= quantidade)
+            return true;
+        else
+            return false;
+    }
+
+    // Inserir um elemento e na posição i – adicionarPosicao(e, i)
+    public void adicionarPosicao(int elemento, int indice) {
+        if(!validarEntrada(indice))
+                System.out.println("Posição inválida!");
+        else {
+            No novoNo = new No(elemento, null);
+            
+            if(indice == 0) {
+                novoNo.setProximo(primeiro);
+                primeiro = novoNo;
+            } else {
+                No noAtual = primeiro;
+
+                for (int i = 0; i < indice - 1; i++)
+                    noAtual = noAtual.getProximo();
+    
+                novoNo.setProximo(noAtual.getProximo());
+                noAtual.setProximo(novoNo);
+            }
+
+            quantidade++;
+        }
+    }
+
+    // Inserir um elemento e no início – adicionarInicio(e)
+    public void adicionarInicio(int elemento) {
+        adicionarPosicao(elemento, 0);
+    }
+
+    // Inserir um elemento e no final – adicionarFinal(e)
+    public void adicionarFinal(int elemento) {
+        adicionarPosicao(elemento, quantidade);
+    }
+
+    // Remover o elemento e na posição i – removerPosicao(i)
+
+    // Remover o elemento no inicio – removerInicio()
+
+    // Remover o elemento no final – removerFinal()
+
+
+
+
+    //Exibir todos os elementos
+    public void exibir(){
+        String elementos = primeiro.getElemento() + ", ";
+
+        No noAtual = primeiro;
+
+        for (int i = 0; i < quantidade - 1; i++) {
+            noAtual = noAtual.getProximo();
+            elementos += noAtual.getElemento();
+
+            if(i != (quantidade - 1))
+                elementos += ", ";
+        }
+        
+        JOptionPane.showMessageDialog(null, elementos);
+    }
+
+}
