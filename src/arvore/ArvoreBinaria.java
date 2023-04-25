bf9bf6926da3afec0786c8f82fedc4b6eb7e7063
PROFESSOR D18 <professord18@acad.unifor.br>
2023-04-25 11:14:09 -0300

    Implementação de buscarPai e nivelElement (incompleto)

--- src/arvore/ArvoreBinaria.java
+++ src/arvore/ArvoreBinaria.java
@@ -40,7 +40,45 @@ public class ArvoreBinaria {
     }
 
     // Descobre o pai de um elemento e – buscarPai(e)
+    public NoAB buscarPai(int elemento) {
+        return buscarPai(raiz, elemento);
+    }
+
+    private NoAB buscarPai(NoAB atual, int elemento) {
+        if(atual.getEsquerda() != null && atual.getEsquerda().getElemento() == elemento)
+            return atual;
+
+        if(atual.getDireita() != null && atual.getDireita().getElemento() == elemento)
+            return atual;
+
+        NoAB resultado = null;
+        if(atual.getEsquerda() != null)
+            resultado = buscarPai(atual.getEsquerda(), elemento);
+        if(resultado == null && atual.getDireita() != null)
+            resultado = buscarPai(atual.getDireita(), elemento);
+        
+        return resultado;
+    }
+
     // Retornar o nível do elemento e – nivelElemento(e)
+    public int nivelElemento(int elemento) {
+        return nivelElemento(raiz, elemento);
+    }
+
+    private int nivelElemento(NoAB atual, int elemento) {
+        if(atual.getElemento() == elemento)
+            return 0;
+        //FIXME corrigir retorno +1
+        int resultado = -1;
+        if(atual.getEsquerda() != null)
+            resultado =  1 + nivelElemento(atual.getEsquerda(), elemento);
+
+        if(resultado == 0 && atual.getDireita() != null)
+            resultado =  1 + nivelElemento(atual.getDireita(), elemento);
+
+        return resultado;
+    }
+
     // Resgatar a altura da árvore – alturaArvore()
     // Desafio: adicionar()
     // Inserir um elemento e à direita de p – adicionarDireita(e, p)
